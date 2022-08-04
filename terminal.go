312c497561d59e9485a981431bc6d84494d51f56
SuLinXin66 <15210204903@163.com>
2022-08-04 20:46:59 +0800

    尝试删除回车拦截

--- terminal.go
+++ terminal.go
@@ -197,7 +197,8 @@ func (t *Terminal) ioloop() {
 				break
 			}
 			isEscape = true
-		case CharInterrupt, CharEnter, CharCtrlJ, CharDelete:
+		//case CharInterrupt, CharEnter, CharCtrlJ, CharDelete:
+		case CharInterrupt, CharCtrlJ, CharDelete:
 			expectNextChar = false
 			fallthrough
 		default:
